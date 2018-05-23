cfdc5d5892f700a06ca25c8ec30d1d76b1e84986
SnowCatPDA <snowcat@vpro.ru>
2018-05-23 23:17:38 +0500

    Android 8.1

--- KIICAA_POWER/BoardConfig.mk
+++ KIICAA_POWER/BoardConfig.mk
@@ -8,10 +8,11 @@ include device/mediatek/mt6580/BoardConfig.mk
 
 #Config partition size
 -include $(MTK_PTGEN_OUT)/partition_size.mk
+ifneq ($(strip $(MTK_AB_OTA_UPDATER)), yes)
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+endif
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-include device/LEAGOO/$(MTK_TARGET_PROJECT)/ProjectConfig.mk
 
 MTK_INTERNAL_CDEFS := $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_NAME),$(if $(filter-out no NO none NONE false FALSE,$($(t))),-D$(t)))
 MTK_INTERNAL_CDEFS += $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_VALUE),$(if $(filter-out no NO none NONE false FALSE,$($(t))),$(foreach v,$(shell echo $($(t)) | tr '[a-z]' '[A-Z]'),-D$(v))))
@@ -24,4 +25,3 @@ BOARD_KERNEL_CMDLINE = bootopt=64S3,32S1,32S1
 else
 BOARD_KERNEL_CMDLINE = bootopt=64S3,32N2,64N2
 endif
-
